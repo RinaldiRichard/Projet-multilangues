e4b4a93e3183cb45ceebb913b2e497d313a64309
Richard Rinaldi <rinaldi.richard@gmail.com>
2021-10-22 15:17:56 +0200

    ajout detection browser language + corectif bg color slider

--- src/context/langContext.js
+++ src/context/langContext.js
@@ -1,14 +1,27 @@
-import React, {createContext, useState} from 'react' 
+import React, { createContext, useState } from "react";
 
 export const Context = createContext();
 
-const ContextProvider = (props) => {
-  const [lang, setLang] = useState("EN");
+const supportedLangs = ['EN', 'FR', 'ES'];
+
+let browserLang = navigator.language.slice(0,2).toUpperCase();
+
+if (supportedLangs.indexOf(browserLang) === -1) {
+  console.log("not supported");
+  browserLang = "EN";
+}
+
+const ContextProvider = props => {
+  const [lang, setLang] = useState(browserLang);
 
   const toggleLang = changeLang => {
-      setLang(changeLang)
-  }
-  return <Context.Provider value={{ lang, toggleLang }}>{props.children}</Context.Provider>;
+    setLang(changeLang);
+  };
+  return (
+    <Context.Provider value={{ lang, toggleLang }}>
+      {props.children}
+    </Context.Provider>
+  );
 };
 
-export default ContextProvider
+export default ContextProvider;
